d05d2329eb1360b5bf3e1749df7fdf119fe7fca4
Anatolii Yakushko <shaddyx@gmail.com>
2018-01-27 15:59:27 +0200

    [+] added star orbits

--- pub/mod/dna/star.js
+++ pub/mod/dna/star.js
@@ -1,31 +1,43 @@
-let Star = function(dat) {
-    this.x = dat.x
-    this.y = dat.y
-    this.m = dat.m
+const ORBIT_DISTANCE = 10;
+const STROKE_STYLE = "blue";
+let Star = function (dat) {
+    this.x = dat.x;
+    this.y = dat.y;
+    this.m = dat.m;
 
     // setup color
-    this.c = dat.c
-    switch(this.c) {
-    case 0: this.img = res['star-red']; break;
-    case 1: this.img = res['star-blue']; break;
-    case 2: this.img = res['star-yellow']; break;
+    this.c = dat.c;
+    this.orbitR = this.m + ORBIT_DISTANCE;
+
+    switch (this.c) {
+        case 0:
+            this.img = res['star-red'];
+            break;
+        case 1:
+            this.img = res['star-blue'];
+            break;
+        case 2:
+            this.img = res['star-yellow'];
+            break;
     }
 
-    _.log.debug('!!!!!!! created star')
-    console.dir(dat)
-}
+    _.log.debug('!!!!!!! created star');
+    console.dir(dat);
+};
 
-Star.prototype.init = function() {
-}
-Star.prototype.evo = function(dt) {
-}
-Star.prototype.draw = function() {
-    ctx.drawImage(this.img, this.x, this.y, this.m, this.m)
+Star.prototype.init = function () {
+};
+Star.prototype.evo = function (dt) {
+};
+Star.prototype.draw = function () {
+    ctx.drawImage(this.img, this.x, this.y, this.m, this.m);
     ctx.beginPath();
-    //ctx.arc(this.x,this.y,50,0,2*Math.PI);
+    ctx.strokeStyle = STROKE_STYLE;
+    ctx.setLineDash([5, 3]);
+    ctx.arc(this.x, this.y, this.orbitR, 0, 2 * Math.PI);
     ctx.stroke();
-}
+};
 
-module.exports = function(dat) {
-    return new Star(dat)
-}
+module.exports = function (dat) {
+    return new Star(dat);
+};
