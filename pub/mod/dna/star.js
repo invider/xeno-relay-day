869c5101daacb3458198e9f7363fe898a448ca75
Anatolii Yakushko <shaddyx@gmail.com>
2018-01-27 16:51:34 +0200

    [+] added antenna rotation

--- pub/mod/dna/star.js
+++ pub/mod/dna/star.js
@@ -8,6 +8,7 @@ let Star = function (dat) {
     // setup color
     this.c = dat.c;
     this.orbitR = this.m + ORBIT_DISTANCE;
+    this.angle = dat.angle;
 
     switch (this.c) {
         case 0:
@@ -20,18 +21,32 @@ let Star = function (dat) {
             this.img = res['star-yellow'];
             break;
     }
+    this.antennaImg = res['star-antenna'];
 
     _.log.debug('!!!!!!! created star');
     console.dir(dat);
 };
 
-Star.prototype.init = function() {
-}
-Star.prototype.evo = function(dt) {
-}
-Star.prototype.draw = function() {
-    ctx.drawImage(this.img, this.x-this.m, this.y-this.m, this.m*2, this.m*2)
+Star.prototype.init = function () {
+};
+Star.prototype.evo = function (dt) {
+};
 
+Star.prototype.drawAntenna = function(){
+    // let antennaX = this.x - this.antennaImg.width / 2;
+    // let antennaY = this.y - this.antennaImg.height / 2;
+    var res = lib.trigonometry.rotateLineAroundFirstDot(this.x, this.y, this.orbitR, this.angle);
+    ctx.save();
+    ctx.translate(res.x1, res.y1);
+    ctx.rotate(Math.PI - this.angle);
+    ctx.drawImage(this.antennaImg, -this.antennaImg.width / 2, -this.antennaImg.height / 2);
+    ctx.restore()
+
+};
+
+Star.prototype.draw = function () {
+    ctx.drawImage(this.img, this.x - this.m, this.y - this.m, this.m * 2, this.m * 2)
+    this.drawAntenna();
     ctx.beginPath();
     ctx.strokeStyle = STROKE_STYLE;
     ctx.setLineDash([5, 3]);
--- pub/mod/setup/loadLevel.js
+++ pub/mod/setup/loadLevel.js
@@ -1,9 +1,23 @@
+let defaults = {
+    angle: 0,
+    maxAngleDiff: 2 * Math.PI,
+    c: 0
+};
+let setDefaults = function(star){
+    var res = Object.assign({}, star);
+    for (var k in defaults){
+        if (res[k] == undefined){
+            res[k] = defaults[k];
+        }
+    }
+    return res;
+};
 module.exports = function(){
     let lvlData = _.lib.levels[$.env.gameInfo.level - 1];
     console.log("Level data:", lvlData);
     for (let k in lvlData.planets){
-        sys.spawn('dna/star', 'lab', lvlData.planets[k]);
+        sys.spawn('dna/star', 'lab', setDefaults(lvlData.planets[k]));
     }
-    sys.spawn('dna/star', 'lab', lvlData.start);
-    sys.spawn('dna/star', 'lab', lvlData.end);
+    sys.spawn('dna/star', 'lab', setDefaults(lvlData.start));
+    sys.spawn('dna/star', 'lab', setDefaults(lvlData.end));
 };
