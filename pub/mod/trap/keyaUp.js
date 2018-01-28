8601c7afb4d3316369bd54ce108354997b20bab9
Anatolii Yakushko <shaddyx@gmail.com>
2018-01-28 12:03:32 +0200

    [+] fixed keys behavior

--- pub/mod/trap/keyaUp.js
+++ pub/mod/trap/keyaUp.js
@@ -1,4 +1,5 @@
 module.exports = function () {
     console.log("rotating")
-    _.lab.start.rotateAntenna(_.lib.constants.ROTATE_ANGLE);
+    //_.lab.start.rotateAntenna(_.lib.constants.ROTATE_ANGLE);
+    _.lab.start.applyCmd(_.lib.constants.commands.ROTATE_LEFT)
 };
