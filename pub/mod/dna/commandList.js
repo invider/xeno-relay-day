06fcf9f66ce36603857c83fd98383b41e03a89f4
Anatolii Yakushko <shaddyx@gmail.com>
2018-01-28 11:53:00 +0200

    [-] fixed commandPalette

--- pub/mod/dna/commandList.js
+++ pub/mod/dna/commandList.js
@@ -3,8 +3,6 @@ const LIST_WIDTH = 500;
 const LIST_HEIGHT = 15;
 const SIGNAL_MARGIN = 3;
 let CommandList = function(dat) {
-    this.x = dat.x;
-    this.y = dat.y;
     //
     //  INFO: mandatory field
     //
@@ -19,27 +17,39 @@ CommandList.prototype.evo = function(dt) {
 };
 
 CommandList.prototype.getItems = function() {
-    return this.star.slice(0);
+    return this.star.sequence.slice(0);
+};
+CommandList.prototype.getItemsCount = function() {
+    return this.star.sequence.length;
 };
 
 
+CommandList.prototype.getStartX = function(){
+    return this.star.x - this.getItemsCount()  * (SIGNAL_WIDTH + SIGNAL_MARGIN)/ 2;
+};
+CommandList.prototype.getStartY = function(){
+    return this.star.y + this.star.orbitR;
+};
+
 CommandList.prototype.drawItem = function(cmd, index){
     ctx.fillStyle = cmd.color;
-    ctx.fillRect(this.x + (index * SIGNAL_WIDTH + SIGNAL_MARGIN), this.y, SIGNAL_WIDTH, SIGNAL_WIDTH);
+    ctx.fillRect(this.getStartX() + (index * SIGNAL_WIDTH + SIGNAL_MARGIN), this.getStartY(), SIGNAL_WIDTH, SIGNAL_WIDTH);
 };
 
 CommandList.prototype.drawItems = function(){
     let items = this.getItems();
     for (var i = 0 ; i<items.length; i++){
-        this.drawItem(items[i]);
+        this.drawItem(items[i], i);
     }
 };
 
 CommandList.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.rect(this.x, this.y, LIST_WIDTH, LIST_HEIGHT);
-    ctx.stroke();
-    ctx.fill();
+    if (this.getItemsCount()){
+        ctx.beginPath();
+        ctx.rect(this.getStartX(), this.getStartY(), this.getItemsCount() * (SIGNAL_WIDTH + SIGNAL_MARGIN), LIST_HEIGHT);
+        ctx.stroke();
+        this.drawItems();
+    }
 };
 
 module.exports = function(dat) {
