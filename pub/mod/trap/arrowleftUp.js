da31c5dd9a8d422801d98c1531d1ccb5bd8520d5
Igor Khotin <khotin@gmx.com>
2018-02-04 21:28:01 +0200

    [*] sfx tuned

--- pub/mod/trap/arrowleftUp.js
+++ pub/mod/trap/arrowleftUp.js
@@ -1,5 +1,5 @@
 module.exports = function () {
     _.lab.start.applyCmd(_.lib.constants.commands.ROTATE_LEFT)
-    lib.sfx('sfx/select-1')
+    lib.sfx('sfx/select-1', 0.4)
     return false;
 };
--- pub/mod/trap/arrowrightUp.js
+++ pub/mod/trap/arrowrightUp.js
@@ -1,5 +1,5 @@
 module.exports = function(){
     _.lab.start.applyCmd(_.lib.constants.commands.ROTATE_RIGHT);
-    lib.sfx('sfx/select-2')
+    lib.sfx('sfx/select-2', 0.4)
     return false;
 };
